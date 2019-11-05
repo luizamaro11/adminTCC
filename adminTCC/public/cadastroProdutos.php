5074e7ed91039a300c34681f3d236ca243cf0c84
luizamaro11 <luiz.amaro90@gmail.com>
2019-11-05 16:01:26 -0300

    mudando o layout do projeto

--- adminTCC/public/cadastroProdutos.php
+++ adminTCC/public/cadastroProdutos.php
@@ -1,112 +1,237 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cadastro Produtos</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/estilo.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Relatorio de Estoque</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
-    <div class="container-fluid">           
-        <div class="row linhaHeader">
-            <div class="col-md-12 cabecalho">
-                <img src="imagens/logo1.png" class="logo">
-                <ul>
-                    <li><a href="mesas.php">Mesas</a></li>
+	<div class="container-fluid">           
+		<div class="row linhaHeader">
+			<div class="col-md-12 cabecalho">
+				<img src="imagens/logo1.png" class="logo">
+				<ul>
+					<li><a href="mesas.php">Mesas</a></li>
                     <li><a href="funcionario.html">Funcionário</a></li>
                     <li><a href="cadastroProdutos.php">Produtos</a></li>
                     <li><a href="relatorioVendas.html">Relatorio de vendas</a></li>
                     <li><a href="relatorioEstoque.html">Relatorios de estoque</a></li>
                     <li><a href="index.php">Sair</a></li>
-                </ul>                   
-            </div>
-        </div>
+				</ul>                   
+			</div>
+        </div>    
         
-        <div class="row borda">
-            <div class="col-md-12">
-        
-            </div>
-        </div>
-        
-        <form>
-            <div class="row titulo">
-                <div class="col-md-12">
-                    <h1>Cadastrar Produtos</h1>
+		<div class="row borda">
+			<div class="col-md-12">
+				
+			</div>
+		</div>
+		
+		<div class="row formTable">
+			<div class="row linhaTable">
+				<div class="col-md-12">
+					<h3>Produtos</h3>
+					<button type="button" class="btn btn-block btn-info" data-toggle="modal" data-target="#adicionarProduto">ADICIONAR PRODUTO</button>
+				</div>
+			</div>
+			
+			<div class="row table-responsive">
+				<div class="col-md-12 funcTable">
+					<table class="table">
+						<thead>
+							<tr>
+								<th scope="col">Nome</th>
+								<th scope="col">Quantidade</th>
+								<th scope="col">tipo</th>
+								<th scope="col">Valor</th>
+								<th scope="col">Ação</th>
+							</tr>
+						</thead>
+						<tbody id="listaProdutos">
+							
+						</tbody>
+					</table>
+				</div>
+			</div>            
+		</div>
+	</div>
+
+	<!--Modal do Botão Adicionar-->
+    <div id="adicionarProduto" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+          
+            <!--Container da modal--> 
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Adicionar Produto</h4>
                 </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-md-8">
-                    <label>Nome do Produto</label>
-                    <input id="nomeProduto" type="text" class="form-control">
+
+                <div class="modal-body">
+					<div class="row">
+						<div class="col-md-6">
+							<label for="" style="color: #000">Nome do produto</label>
+							<input id="nomeProduto" type="text" class="form-control" name="nmProduto" placeholder="digite o nome da mesa">
+						</div>
+
+						<div class="col-md-6">
+							<label style="color: #000">Quantidade</label>
+							<input id="quantidadeProduto" type="number" class="form-control">
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-6">
+							<label style="color: #000">Preço do Produto</label>
+							<input id="valorProduto" type="number" class="form-control">	
+						</div>
+
+						<div class="col-md-6">							
+							<label style="color: #000">Tipo de Produto</label>
+							<select id="tipoUnidadeProduto" class="form-control">
+								<option>unidades</option>
+								<option>porção</option>
+								<option>litros</option>
+								<option>kg</option>
+							</select>	
+						</div>
+					</div>
                 </div>
                 
-                <div class="col-md-4">
-                    <label>Quantidade</label>
-                    <input id="quantidadeProduto" type="number" class="form-control">
+                <div class="modal-footer">
+                    <button id="salvarProduto" type="button" class="btn btn-default btn-success" data-dismiss="modal">SALVAR</button>
+                    <button type="button" class="btn btn-default btn-danger" data-dismiss="modal">CANCELAR</button>
                 </div>
             </div>
-            
-            <div class="">
-                <label>Preço do Produto</label>
-                <input id="valorProduto" type="number" class="form-control">
-            </div>
-            
-            <div class="">
-                <label>Tipo de Produto</label>
-                <select id="tipoUnidadeProduto" class="form-control">
-                    <option>unidades</option>
-                    <option>porção</option>
-                    <option>litros</option>
-                    <option>kg</option>
-                </select>
-            </div>
-            
-            <div class="botao">
-                <button class="btn btn-block" id="salvarProduto">SALVAR</button>
-            </div>
-        </form>
+        </div>
     </div>
-    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript">
+</body>
+<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript">
+
+    let nomeProduto = document.querySelector("#nomeProduto");
+    let quantidadeProduto = document.querySelector("#quantidadeProduto");
+    let valorProduto = document.querySelector("#valorProduto");
+    let tipoUnidadeProduto = document.querySelector("#tipoUnidadeProduto");
+    let btnSalvar = document.querySelector("#salvarProduto");
         
-        let nomeProduto = document.querySelector("#nomeProduto");
-        let quantidadeProduto = document.querySelector("#quantidadeProduto");
-        let valorProduto = document.querySelector("#valorProduto");
-        let tipoUnidadeProduto = document.querySelector("#tipoUnidadeProduto");
-        let btnSalvar = document.querySelector("#salvarProduto");
-        
-        btnSalvar.addEventListener('click', event => {
+    btnSalvar.addEventListener('click', event => {
            
-            $.ajax({
-               url:'php_action/createProduto.php',
-               type:'POST',
-               data: {
-                   nomeProduto: nomeProduto.value,
-                   quantidadeProduto: quantidadeProduto.value,
-                   valorProduto: valorProduto.value,
-                   tipoUnidadeProduto: tipoUnidadeProduto.selectedOptions[0].value,
-               },
-               success: (data) => {
+        $.ajax({
+            url:'php_action/createProduto.php',
+            type:'POST',
+            data: {
+                nomeProduto: nomeProduto.value,
+                quantidadeProduto: quantidadeProduto.value,
+                valorProduto: valorProduto.value,
+                tipoUnidadeProduto: tipoUnidadeProduto.selectedOptions[0].value,
+            },
+
+            success: (data) => {
                 
                 if(data == 1){
                    
                     alert('Produto Cadastrado com Sucesso');
                    
-                }else{
+                } else {
                     
                     alert('Falha ao Cadastrar o Número da Mesa');
                     
                 }
                 
-               }
-            });
-            
+            }
         });
+            
+    });
+       
+    $(document).ready(function (){
+           
+        retornaProdutos();
+            
+    });
         
-    </script>
-</body>
+    function retornaProdutos(){
+            
+        $.ajax({
+            url:'php_action/listarProdutos.php',
+            type:'POST',
+            success: (data) => {
+                        
+                let produtos = JSON.parse(data);
+                let linhaProduto;
+                let contadorProduto = 0;
+                            
+                while (contadorProduto < produtos.length){
+                                
+                    linhaProduto += `
+                        <tr>
+						    <th scope="row">${produtos[contadorProduto].nm_produto}</th>
+							<th>${(produtos[contadorProduto].qt_produto == null) ? 0 : produtos[contadorProduto].qt_produto}</th>
+							<th>${produtos[contadorProduto].tp_produto}</th>
+							<th>${produtos[contadorProduto].vl_produto}</th>
+							<td>
+								<button class="btn-block btn-warning" onclick="alterarProdutos">Alterar</button>
+							</td>
+						</tr>`;
+                                
+                    contadorProduto++;
+                                
+                }
+                            
+                document.querySelector("#listaProdutos").innerHTML = linhaProduto;
+                          
+            } 
+            
+        });       
+    }
+        
+    function alterarProdutos(codigoProduto){
+                
+        $.ajax({
+            url:'php_action/alterarProdutos.php',
+            type:'POST',
+            data: {codigoProduto: codigoProduto},
+            success: (data) => {
+                
+                if(data == 1){
+                    
+                    alert("produto alterado com sucesso");
+                    retornaProdutos();
+                    
+                } else {
+                    
+                    alert("Falha ao alterar as informações do produto");
+                    
+                }
+                
+            }
+        });
+ 
+        $.ajax({
+            url:'php_action/alterarProdutos.php',
+            type:'POST',
+            data: {codigoProduto: codigoProduto},
+            success: (data) => {
+                
+                if(data == 1){
+                    
+                    alert("produto alterado com sucesso");
+                    retornaProdutos();
+                    
+                } else {
+                    
+                    alert("Falha ao alterar as informações do produto");
+                    
+                }
+                
+            }
+        });
+            
+    }
+	
+</script>
 </html>
